60bcc4c63e58b7f34733da7ff82fb4c8097f1b8c
WedadSulaiman <wedad4521@gmail.com>
2026-03-01 21:50:10 +0300

    Lab4 Dgango

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 def index(request):
-    name = request.GET.get("name") or "world!"
-    return render(request, "bookmodule/index.html",{"name":name})
+    return render(request, "bookmodule/index.html")
 
-def index2(request, val1):
-    return HttpResponse("value1 = " +str(val1)) 
+def list_books(request):
+    return render(request, "bookmodule/list_books.html") 
 
 def viewbook(request,bookId):
-    book1 = {'id':123, 'title':'Continuous Delivery', 'author':'J.Humble and D.Farley'}
-    book2 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E.Eilam'}
-    targetBook = None
-    if book1['id']== bookId : targetBook = book1
-    if book2['id']== bookId : targetBook = book2
-    context={'book':targetBook}
-    return render(request,'bookmodule/show.html',context)
+    return render(request,'bookmodule/one_book.html')
+
+def aboutus(request):
+    return render(request, "bookmodule/aboutus.html")
